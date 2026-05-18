49aa62f5f2b27d4d359fa20d6260b1967261966c
dhruvaibuilds <dhruv.aibuilds@gmail.com>
2026-05-18 21:57:04 +0100

    Update band_name_generator.py

--- band_name_generator.py
+++ band_name_generator.py
@@ -3,4 +3,4 @@
 print("Welcome to the Band Name Generator")
 city = input("What's the name of the city you grew up in?" + "\n")
 pet_name = input("What's your pet's name?" + "\n")
-print("Your band name could be: " + city + " " + pet_name + ".")
+print("Your band name could be: " + city + " " + pet_name)
